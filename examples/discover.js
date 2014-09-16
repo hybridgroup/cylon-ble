56be93d402b32e90b6ea1f29e2c149b9325135f3
deadprogram <ron@hybridgroup.com>
2014-09-15 17:24:15 -0700

    More 'generic'

--- examples/discover.js
+++ examples/discover.js
@@ -2,11 +2,11 @@ var Cylon = require('cylon');
 
 Cylon.robot({
   connection: { name: 'bluetooth', adaptor: 'ble', uuid: '207377654321'},
-  device: {name: 'generic', driver: 'generic', serviceId: '1800', characteristicId: '2a00'},
+  device: {name: 'generic', driver: 'generic-access'},
 
   work: function(my) {
     after((1).second(), function() {
-      my.generic.getServiceCharacteristic(function(err, data){
+      my.generic.getName(function(err, data){
         if (err) {
           console.log(err);
         } else {
