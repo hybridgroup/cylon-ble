90c79a55cd1000c6b4c7519372a94327f4ab5138
deadprogram <ron@hybridgroup.com>
2014-09-21 20:55:15 -0700

    Can read sensor characteristic data for WICED Sense

---
+++ test/specs/wiced-sense.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var WICEDSense = source("wiced-sense");
+
+describe("Cylon.Drivers.BLE.WICEDSense", function() {
+  var driver = new WICEDSense({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
