d9c5b54fdab725160eaeb32e51dc767f1b5a1047
Andrew Stewart <andrew@stwrt.ca>
2014-10-14 09:21:19 -0700

    Improve spec coverage for cylon-ble.js

--- test/specs/cylon-ble.spec.js
+++ test/specs/cylon-ble.spec.js
@@ -2,37 +2,44 @@
 
 var module = source("cylon-ble");
 
-var Central = source('central'),
-    BLEBatteryService = source('battery-service'),
-    BLEDeviceInformation = source('device-information'),
-    BLEGenericAccess = source('generic-access')
+var Central = source('central');
+
+var Drivers = {
+  'ble-battery-service': source('battery-service'),
+  'ble-generic-access': source('generic-access'),
+  'ble-device-information': source('device-information')
+};
 
 describe("Cylon.BLE", function() {
   describe("#register", function() {
-    it("should be a function", function() {
-      expect(module.register).to.be.a('function');
+    var bot;
+
+    beforeEach(function() {
+      bot = { registerDriver: spy(), registerAdaptor: spy() };
+      module.register(bot);
+    });
+
+    it("should register the drivers with the Robot", function() {
+      for (var driver in Drivers) {
+        expect(bot.registerDriver).to.be.calledWith("cylon-ble", driver);
+      };
+    });
+
+    it("should register the 'central' adaptor with the Robot", function() {
+      expect(bot.registerAdaptor).to.be.calledWith("cylon-ble", 'central');
     });
   });
 
   describe("#driver", function() {
-    var opts = { device: {connection: 'test'}, extraParams: {} };
+    var opts = { device: { connection: 'test' }, extraParams: {} };
 
-    it("can instantiate a new BLEBatteryService", function() {
-      opts.name = 'ble-battery-service';
-      var driver = module.driver(opts);
-      expect(driver).to.be.an.instanceOf(BLEBatteryService);
-    });
+    it("instantiates BLE drivers", function() {
+      for (var driver in Drivers) {
+        var driverClass = Drivers[driver];
+        opts.name = driver;
 
-    it("can instantiate a new BLEDeviceInformation", function() {
-      opts.name = 'ble-device-information';
-      var driver = module.driver(opts);
-      expect(driver).to.be.an.instanceOf(BLEDeviceInformation);
-    });
-
-    it("can instantiate a new BLEGenericAccess", function() {
-      opts.name = 'ble-generic-access';
-      var driver = module.driver(opts);
-      expect(driver).to.be.an.instanceOf(BLEGenericAccess);
+        expect(module.driver(opts)).to.be.an.instanceOf(driverClass);
+      }
     });
   });
 
