1036c99cd87d612a150f34612212215799f869e6
Andrew Stewart <andrew@stwrt.ca>
2014-10-22 12:30:43 -0700

    Add discovery utility

---
+++ util/discover.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var noble = require('noble');
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    console.log("Starting scan.");
+    noble.startScanning();
+  } else {
+    console.log("Stopping scan.");
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+  console.log("Peripheral discovered!")
+  console.log("  Name: " + peripheral.advertisement.localName)
+  console.log("  UUID: " + peripheral.uuid);
+});
