ce63c4afc262a9627903c49fb2a9c0e3f28ac960
deadprogram <ron@hybridgroup.com>
2015-08-03 17:43:58 -0700

    Direct BLE characteristic driver

---
+++ examples/characteristic/characteristic.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    bluetooth: { adaptor: 'central', uuid: '207377654321', module: 'cylon-ble' }
+  },
+
+  devices: {
+    wiced: { driver: 'ble-characteristic', 
+             serviceId: "180f", 
+             characteristicId: "2a19",
+             connection: "bluetooth" }
+  },
+
+  work: function(my) {
+    my.wiced.readCharacteristic(function(err, data) {
+      if (!!err) {
+        console.log("Error: ", err);
+        return;
+      }
+
+      console.log("Data: ", data);
+    });
+  }
+}).start();
+
