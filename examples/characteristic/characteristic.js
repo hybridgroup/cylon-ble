554b85444b34db6cb155202e9be71233c61643dd
deadprogram <ron@hybridgroup.com>
2015-08-03 17:48:46 -0700

    Cleanup for eslint

--- examples/characteristic/characteristic.js
+++ examples/characteristic/characteristic.js
@@ -1,15 +1,15 @@
 "use strict";
 
-var Cylon = require('cylon');
+var Cylon = require("cylon");
 
 Cylon.robot({
   connections: {
-    bluetooth: { adaptor: 'central', uuid: '207377654321', module: 'cylon-ble' }
+    bluetooth: { adaptor: "central", uuid: "207377654321", module: "cylon-ble" }
   },
 
   devices: {
-    wiced: { driver: 'ble-characteristic', 
-             serviceId: "180f", 
+    wiced: { driver: "ble-characteristic",
+             serviceId: "180f",
              characteristicId: "2a19",
              connection: "bluetooth" }
   },
