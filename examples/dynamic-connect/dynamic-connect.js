f0407ca1aeafcd889d0a0f5faf3c7ff911d4a8f7
deadprogram <ron@hybridgroup.com>
2015-09-06 11:43:40 -0700

    Last things needed for dynamic connect

---
+++ examples/dynamic-connect/dynamic-connect.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var Cylon = require("cylon");
+
+Cylon.robot({
+  connections: {
+    bluetooth: { adaptor: "central", module: __dirname + "/../../" }
+  },
+
+  connectBLE: function(peripheral) {
+    if (this.connected) { return; }
+
+    console.log(peripheral.advertisement.localName, peripheral.uuid);
+    this.bluetooth.connectPeripheral(peripheral.uuid, peripheral, function() {
+      this.connected = true;
+      this.device("blething",
+                  {connection: "bluetooth", driver: "ble-device-information"});
+      this.devices.blething.getManufacturerName(function(err, data) {
+        if (err) {
+          console.log("error: ", err);
+          return;
+        }
+        console.log("data: ", data);
+      });
+    }.bind(this));
+  },
+
+  work: function(my) {
+    this.connected = false;
+
+    my.bluetooth.on("discover", function(peripheral) {
+      my.connectBLE(peripheral);
+    });
+  }
+}).start();
